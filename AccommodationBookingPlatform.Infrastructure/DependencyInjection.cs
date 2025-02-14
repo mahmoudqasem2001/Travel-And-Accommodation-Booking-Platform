2a2a88eeca9fbabafed05b7d10f4f3f3bb7e0b40
mahmoudqasem2001 <mahmoodqasim543@gmail.com>
2025-02-14 23:30:39 +0200

    OPS-8 Implement The Architecture

---
+++ AccommodationBookingPlatform.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AccommodationBookingPlatform.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(
+    this IServiceCollection services,
+    IConfiguration config)
+        {
+            ArgumentNullException.ThrowIfNull(config);
+            //services.AddPersistenceInfrastructure(config)
+            //  .AddAuthInfrastructure()
+            //  .AddEmailInfrastructure()
+            //  .AddPdfInfrastructure()
+            //  .AddTransient<IDateTimeProvider, DateTimeProvider>();
+
+            return services;
+        }
+    }
+}
