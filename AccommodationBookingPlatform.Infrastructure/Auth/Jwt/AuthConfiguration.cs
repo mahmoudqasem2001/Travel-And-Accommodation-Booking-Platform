e4fc92106f98a8e888c6da73d04ffde95bde6fd7
mahmoudqasem2001 <mahmoodqasim543@gmail.com>
2025-02-22 22:01:31 +0200

    OPS-10 Infrastructure Layer: Implement Auth using JWT

---
+++ AccommodationBookingPlatform.Infrastructure/Auth/Jwt/AuthConfiguration.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+using AccommodationBookingPlatform.Domain.Interfaces.Auth;
+using AccommodationBookingPlatform.Shared.OptionsValidation;
+using FluentValidation;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+
+namespace AccommodationBookingPlatform.Infrastructure.Auth.Jwt;
+
+public static class AuthConfiguration
+{
+    public static IServiceCollection AddAuthInfrastructure(
+      this IServiceCollection services)
+    {
+        services.AddScoped<IValidator<JwtAuthConfig>, JwtAuthConfigValidator>();
+
+        services.AddOptions<JwtAuthConfig>()
+          .BindConfiguration(nameof(JwtAuthConfig))
+          .ValidateFluentValidation()
+          .ValidateOnStart();
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options =>
+        {
+            using var scope = services.BuildServiceProvider().CreateScope();
+
+            var config = scope.ServiceProvider
+          .GetRequiredService<IOptions<JwtAuthConfig>>().Value;
+
+            var key = Encoding.UTF8.GetBytes(config.Key);
+
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = config.Issuer,
+                ValidAudience = config.Audience,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ClockSkew = TimeSpan.Zero
+            };
+        });
+
+        services.AddTransient<IJwtTokenGenerator, JwtTokenGenerator>();
+
+        return services;
+    }
+}
---
+++ AccommodationBookingPlatform.Infrastructure/Auth/Jwt/JwtAuthConfig.cs
@@ -0,0 +1,12 @@
+namespace AccommodationBookingPlatform.Infrastructure.Auth.Jwt;
+
+public class JwtAuthConfig
+{
+    public required string Key { get; set; }
+
+    public required string Issuer { get; set; }
+
+    public required string Audience { get; set; }
+
+    public required double LifetimeMinutes { get; set; }
+}
