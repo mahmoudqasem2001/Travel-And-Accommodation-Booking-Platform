89485b34f22d999c0141b361de56f0b232b60d3a
mahmoudqasem2001 <mahmoodqasim543@gmail.com>
2025-02-22 22:06:56 +0200

    OPS-10 Domain Layer: Implement Auth Interfaces (IJwTokenGenerator)

---
+++ AccommodationBookingPlatform.Domain/Interfaces/Auth/IJwtTokenGenerator.cs
@@ -0,0 +1,10 @@
+﻿using AccommodationBookingPlatform.Domain.Entities;
+using Domain.Models;
+
+
+namespace AccommodationBookingPlatform.Domain.Interfaces.Auth;
+
+public interface IJwtTokenGenerator
+{
+    JwtToken Generate(User user);
+}
