07086f88112e4bdf0a8df342ea972215cb21969e
mahmoudqasem2001 <mahmoodqasim543@gmail.com>
2025-02-22 22:03:57 +0200

    OPS-10 Infrastrucure Layer: Implement Hotel DbContext

---
+++ AccommodationBookingPlatform.Infrastructure/Presistence/DbContexts/HotelBookingDbContext.cs
@@ -0,0 +1,42 @@
+using AccommodationBookingPlatform.Domain.Entities;
+using Infrastructure.Persistence.Configurations;
+using Infrastructure.Presistence.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace AccommodationBookingPlatform.Infrastructure.Presistence.DbContexts;
+
+public class HotelBookingDbContext(DbContextOptions<HotelBookingDbContext> options) : DbContext(options)
+{
+    public DbSet<Amenity> Amenities { get; set; }
+    public DbSet<Booking> Bookings { get; set; }
+    public DbSet<City> Cities { get; set; }
+    public DbSet<Discount> Discounts { get; set; }
+    public DbSet<Hotel> Hotels { get; set; }
+    public DbSet<Image> Images { get; set; }
+    public DbSet<Owner> Owners { get; set; }
+    public DbSet<Review> Reviews { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<Room> Rooms { get; set; }
+    public DbSet<RoomClass> RoomClasses { get; set; }
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new AmenityConfiguration());
+        modelBuilder.ApplyConfiguration(new BookingConfiguration());
+        modelBuilder.ApplyConfiguration(new CityConfiguration());
+        modelBuilder.ApplyConfiguration(new DiscountConfiguration());
+        modelBuilder.ApplyConfiguration(new HotelConfiguration());
+        modelBuilder.ApplyConfiguration(new ImageConfiguration());
+        modelBuilder.ApplyConfiguration(new InvoiceRecordConfiguration());
+        modelBuilder.ApplyConfiguration(new OwnerConfiguration());
+        modelBuilder.ApplyConfiguration(new ReviewConfiguration());
+        modelBuilder.ApplyConfiguration(new RoleConfiguration());
+        modelBuilder.ApplyConfiguration(new RoomClassConfiguration());
+        modelBuilder.ApplyConfiguration(new RoomConfiguration());
+        modelBuilder.ApplyConfiguration(new UserConfiguration());
+
+    }
+}
