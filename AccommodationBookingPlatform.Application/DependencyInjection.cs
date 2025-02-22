65f7b93b05f2102ca3c6875320e02af2be35eb61
mahmoudqasem2001 <mahmoodqasim543@gmail.com>
2025-02-22 21:57:33 +0200

    OPS-10 Application Layer: Implement Registeration Functionality Using Midiator

--- AccommodationBookingPlatform.Application/DependencyInjection.cs
+++ AccommodationBookingPlatform.Application/DependencyInjection.cs
@@ -8,9 +8,9 @@ namespace AccommodationBookingPlatform.Application
     {
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
-            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+            //services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
 
-            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            //services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
             return services;
         }
