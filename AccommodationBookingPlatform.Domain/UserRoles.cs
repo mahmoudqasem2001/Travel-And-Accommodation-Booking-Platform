3f4684ce1500ff9411beca6d50c274c2d90b1e11
mahmoudqasem2001 <mahmoodqasim543@gmail.com>
2025-02-22 22:00:42 +0200

    OPS-10 Donmain Layer: Implement UserRoles

---
+++ AccommodationBookingPlatform.Domain/UserRoles.cs
@@ -0,0 +1,8 @@
+﻿namespace AccommodationBookingPlatform.Domain
+{
+    public static class UserRoles
+    {
+        public const string Guest = "Guest";
+        public const string Admin = "Admin";
+    }
+}
