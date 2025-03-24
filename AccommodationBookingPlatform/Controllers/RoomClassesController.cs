e58ca301b241a4022d2dd50d6b569966d0db9551
mahmoudqasem2001 <mahmoodqasim543@gmail.com>
2025-03-24 22:26:39 +0200

    OSP-16 Implement Rooms and RoomClasses Contrallers

---
+++ AccommodationBookingPlatform/Controllers/RoomClassesController.cs
@@ -0,0 +1,122 @@
+﻿using AccommodationBookingPlatform.Application.RoomClasses.AddImageToGallery;
+using AccommodationBookingPlatform.Application.RoomClasses.Create;
+using AccommodationBookingPlatform.Application.RoomClasses.Delete;
+using AccommodationBookingPlatform.Application.RoomClasses.GetForManagement;
+using AccommodationBookingPlatform.Application.RoomClasses.Update;
+using AccommodationBookingPlatform.Domain;
+using AccommodationBookingPlatform.DTOs.Images;
+using AccommodationBookingPlatform.DTOs.RoomClasses;
+using AccommodationBookingPlatform.Extensions;
+using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace AccommodationBookingPlatform.Controllers;
+
+[ApiController]
+[Route("api/room-classes")]
+[ApiVersion("1.0")]
+[Authorize(Roles = UserRoles.Admin)]
+public class RoomClassesController(ISender mediator, IMapper mapper) : ControllerBase
+{
+   
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<RoomClassForManagementResponse>>> GetRoomClassesForManagement(
+      [FromQuery] RoomClassesGetRequest roomClassesGetRequest,
+      CancellationToken cancellationToken)
+    {
+        var query = mapper.Map<GetRoomClassesForManagementQuery>(roomClassesGetRequest);
+
+        var roomClasses = await mediator.Send(query, cancellationToken);
+
+        Response.Headers.AddPaginationMetadata(roomClasses.PaginationMetadata);
+
+        return Ok(roomClasses.Items);
+    }
+
+  
+    /// <response code="409">If there is a room class with the same name in the same hotel.</response>
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status409Conflict)]
+    [HttpPost]
+    public async Task<IActionResult> CreateRoomClass(
+      RoomClassCreationRequest roomClassCreationRequest,
+      CancellationToken cancellationToken)
+    {
+        var command = mapper.Map<CreateRoomClassCommand>(roomClassCreationRequest);
+
+        await mediator.Send(command, cancellationToken);
+
+        return Created();
+    }
+
+    
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status409Conflict)]
+    [HttpPut("{id:guid}")]
+    public async Task<IActionResult> UpdateRoomClass(
+      Guid id,
+      RoomClassUpdateRequest roomClassUpdateRequest,
+      CancellationToken cancellationToken)
+    {
+        var command = new UpdateRoomClassCommand { RoomClassId = id };
+        mapper.Map(roomClassUpdateRequest, command);
+
+        await mediator.Send(command, cancellationToken);
+
+        return NoContent();
+    }
+
+  
+    /// <response code="409">If there are rooms in the room class.</response>
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status409Conflict)]
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> DeleteRoomClass(
+      Guid id,
+      CancellationToken cancellationToken)
+    {
+        var command = new DeleteRoomClassCommand { RoomClassId = id };
+
+        await mediator.Send(command, cancellationToken);
+
+        return NoContent();
+    }
+
+    
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [HttpPost("{id:guid}/gallery")]
+    public async Task<IActionResult> AddImageToHotelGallery(
+      Guid id,
+      [FromForm] ImageCreationRequest imageCreationRequest,
+      CancellationToken cancellationToken)
+    {
+        var command = new AddImageToRoomClassGalleryCommand { RoomClassId = id };
+        mapper.Map(imageCreationRequest, command);
+
+        await mediator.Send(command, cancellationToken);
+
+        return NoContent();
+    }
+}
---
+++ AccommodationBookingPlatform/Controllers/RoomsController.cs
@@ -0,0 +1,136 @@
+﻿using AccommodationBookingPlatform.Application.Rooms.Create;
+using AccommodationBookingPlatform.Application.Rooms.Delete;
+using AccommodationBookingPlatform.Application.Rooms.GetByRoomClassIdForGuest;
+using AccommodationBookingPlatform.Application.Rooms.GetForManagement;
+using AccommodationBookingPlatform.Application.Rooms.Update;
+using AccommodationBookingPlatform.Domain;
+using AccommodationBookingPlatform.DTOs.Rooms;
+using AccommodationBookingPlatform.Extensions;
+using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace AccommodationBookingPlatform.Controllers;
+
+[ApiController]
+[Route("api/room-classes/{roomClassId:guid}/rooms")]
+[ApiVersion("1.0")]
+[Authorize(Roles = UserRoles.Admin)]
+public class RoomsController(ISender mediator, IMapper mapper) : ControllerBase
+{
+  
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<RoomForManagementResponse>>> GetRoomsForManagement(
+      Guid roomClassId,
+      [FromQuery] DTOs.Rooms.RoomsGetRequest roomsGetRequest,
+      CancellationToken cancellationToken)
+    {
+        var query = new GetRoomsForManagementQuery { RoomClassId = roomClassId };
+        mapper.Map(roomsGetRequest, query);
+
+        var rooms = await mediator.Send(query, cancellationToken);
+
+        Response.Headers.AddPaginationMetadata(rooms.PaginationMetadata);
+
+        return Ok(rooms.Items);
+    }
+
+
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [HttpGet("available")]
+    [AllowAnonymous]
+    public async Task<ActionResult<IEnumerable<RoomForGuestResponse>>> GetRoomsForGuests(
+      Guid roomClassId,
+      [FromQuery] RoomsForGuestsGetRequest roomsForGuestsGetRequest,
+      CancellationToken cancellationToken)
+    {
+        var query = new GetRoomsByRoomClassIdForGuestsQuery { RoomClassId = roomClassId };
+        mapper.Map(roomsForGuestsGetRequest, query);
+
+        var rooms = await mediator.Send(query, cancellationToken);
+
+        Response.Headers.AddPaginationMetadata(rooms.PaginationMetadata);
+
+        return Ok(rooms.Items);
+    }
+
+    
+    /// <response code="409">If there is a room with the same number in the room class of the room class.</response>
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status409Conflict)]
+    [HttpPost]
+    public async Task<IActionResult> CreateRoomInRoomClass(
+      Guid roomClassId,
+      RoomCreationRequest roomCreationRequest,
+      CancellationToken cancellationToken)
+    {
+        var command = new CreateRoomCommand { RoomClassId = roomClassId };
+        mapper.Map(roomCreationRequest, command);
+
+        await mediator.Send(command, cancellationToken);
+
+        return Created();
+    }
+
+
+    /// <response code="409">If there is a room with the same number in the room class of the room class.</response>
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status409Conflict)]
+    [HttpPut("{id:guid}")]
+    public async Task<IActionResult> UpdateRoomInRoomClass(
+      Guid roomClassId, Guid id,
+      RoomUpdateRequest roomUpdateRequest,
+      CancellationToken cancellationToken)
+    {
+        var command = new UpdateRoomCommand
+        {
+            RoomClassId = roomClassId,
+            RoomId = id
+        };
+        mapper.Map(roomUpdateRequest, command);
+
+        await mediator.Send(command, cancellationToken);
+
+        return NoContent();
+    }
+
+
+    /// <response code="409">If there are bookings to the room.</response>
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status409Conflict)]
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> DeleteRoomInRoomClass(Guid roomClassId,
+      Guid id,
+      CancellationToken cancellationToken = default)
+    {
+        var command = new DeleteRoomCommand
+        {
+            RoomClassId = roomClassId,
+            RoomId = id
+        };
+
+        await mediator.Send(command, cancellationToken);
+
+        return NoContent();
+    }
+}
