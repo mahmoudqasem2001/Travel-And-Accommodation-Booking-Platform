45e005a13a43acf8d4b58c477a7d1c2baf96bf22
mahmoudqasem2001 <mahmoodqasim543@gmail.com>
2025-04-01 00:57:41 +0200

    OPS-23 Handle Reviews Mapping Profiles

---
+++ AccommodationBookingPlatform.Application/Mapping/ReviewProfile.cs
@@ -0,0 +1,20 @@
+﻿using AccommodationBookingPlatform.Application.Reviews.Common;
+using AccommodationBookingPlatform.Application.Reviews.Create;
+using AccommodationBookingPlatform.Application.Reviews.Update;
+using AccommodationBookingPlatform.Domain.Entities;
+using AutoMapper;
+
+
+namespace AccommodationBookingPlatform.Application.Mapping;
+
+public class ReviewProfile : Profile
+{
+    public ReviewProfile()
+    {
+        CreateMap<CreateReviewCommand, Review>();
+        CreateMap<UpdateReviewCommand, Review>();
+        CreateMap<Review, ReviewResponse>();
+        CreateMap<PaginatedList<Review>, PaginatedList<ReviewResponse>>()
+          .ForMember(dst => dst.Items, options => options.MapFrom(src => src.Items));
+    }
+}
---
+++ AccommodationBookingPlatform/Mapping/ReviewProfile.cs
@@ -0,0 +1,21 @@
+﻿using AccommodationBookingPlatform.Application.Reviews.Create;
+using AccommodationBookingPlatform.Application.Reviews.GetByHotelId;
+using AccommodationBookingPlatform.Application.Reviews.Update;
+using AccommodationBookingPlatform.DTOs.Reviews;
+using AutoMapper;
+using static AccommodationBookingPlatform.Mapping.MappingUtilities;
+
+namespace AccommodationBookingPlatform.Mapping;
+
+public class ReviewProfile : Profile
+{
+    public ReviewProfile()
+    {
+        CreateMap<ReviewsGetRequest, GetReviewsByHotelIdQuery>()
+          .ForMember(dst => dst.SortOrder, opt => opt.MapFrom(src => MapToSortOrder(src.SortOrder)));
+
+        CreateMap<ReviewCreationRequest, CreateReviewCommand>();
+
+        CreateMap<ReviewUpdateRequest, UpdateReviewCommand>();
+    }
+}
