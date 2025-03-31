d309453154b5694e5add19c875543a961d995ceb
mahmoudqasem2001 <mahmoodqasim543@gmail.com>
2025-04-01 00:39:41 +0200

    OPS-22 Implement City Mapping Profiles

---
+++ AccommodationBookingPlatform/Mapping/CityProfile.cs
@@ -0,0 +1,24 @@
+﻿using AccommodationBookingPlatform.Application.Cities.Create;
+using AccommodationBookingPlatform.Application.Cities.GetForManagement;
+using AccommodationBookingPlatform.Application.Cities.GetTrending;
+using AccommodationBookingPlatform.Application.Cities.Update;
+using AccommodationBookingPlatform.DTOs.Cities;
+using AutoMapper;
+using static AccommodationBookingPlatform.Mapping.MappingUtilities;
+
+namespace AccommodationBookingPlatform.Mapping;
+
+public class CityProfile : Profile
+{
+    public CityProfile()
+    {
+        CreateMap<CitiesGetRequest, GetCitiesForManagementQuery>()
+          .ForMember(dst => dst.SortOrder, opt => opt.MapFrom(src => MapToSortOrder(src.SortOrder)));
+
+        CreateMap<TrendingCitiesGetRequest, GetTrendingCitiesQuery>();
+
+        CreateMap<CityCreationRequest, CreateCityCommand>();
+
+        CreateMap<CityUpdateRequest, UpdateCityCommand>();
+    }
+}
