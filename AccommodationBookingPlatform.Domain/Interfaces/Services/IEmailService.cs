55bf5f59af6fb2540637e8205f433b1ab83ef696
mahmoudqasem2001 <mahmoodqasim543@gmail.com>
2025-04-01 16:05:46 +0200

    OPS-21 Add IEmail Service

---
+++ AccommodationBookingPlatform.Domain/Interfaces/Services/IEmailService.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Models;
+
+namespace Domain.Interfaces.Services;
+
+public interface IEmailService
+{
+  Task SendAsync(EmailRequest emailRequest, CancellationToken cancellationToken = default);
+}
