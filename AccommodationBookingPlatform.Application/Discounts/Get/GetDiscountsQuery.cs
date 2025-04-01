e44b0a9bb4588ced76bda993b70fc3f73770746a
mahmoudqasem2001 <mahmoodqasim543@gmail.com>
2025-04-01 10:57:15 +0200

    OPS-24 Implement Discount Commands and Handlers

---
+++ AccommodationBookingPlatform.Application/Discounts/Get/GetDiscountsQuery.cs
@@ -0,0 +1,15 @@
+﻿using AccommodationBookingPlatform.Application.Discounts.GetById;
+using AccommodationBookingPlatform.Domain.Enums;
+using MediatR;
+
+
+namespace AccommodationBookingPlatform.Application.Discounts.Get;
+
+public class GetDiscountsQuery : IRequest<PaginatedList<DiscountResponse>>
+{
+    public Guid RoomClassId { get; init; }
+    public SortOrder? SortOrder { get; init; }
+    public string? SortColumn { get; init; }
+    public int PageNumber { get; init; }
+    public int PageSize { get; init; }
+}
---
+++ AccommodationBookingPlatform.Application/Discounts/GetById/GetDiscountByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace AccommodationBookingPlatform.Application.Discounts.GetById;
+
+public class GetDiscountByIdQuery : IRequest<DiscountResponse>
+{
+    public Guid RoomClassId { get; init; }
+    public Guid DiscountId { get; init; }
+}
