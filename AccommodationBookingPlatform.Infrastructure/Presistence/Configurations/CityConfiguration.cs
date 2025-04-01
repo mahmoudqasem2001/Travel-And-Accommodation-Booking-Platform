098d1c922385c3f2f7be9dbc6f5d7fd284b89e26
mahmoudqasem2001 <mahmoodqasim543@gmail.com>
2025-04-01 21:19:42 +0200

    OPS-22 Setup Cities Configuration

---
+++ AccommodationBookingPlatform.Infrastructure/Presistence/Configurations/CityConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using AccommodationBookingPlatform.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Presistence.Configurations;
+
+public class CityConfiguration : IEntityTypeConfiguration<City>
+{
+    public void Configure(EntityTypeBuilder<City> builder)
+    {
+        builder.HasKey(c => c.Id);
+
+        builder.HasMany(c => c.Hotels)
+          .WithOne(h => h.City)
+          .IsRequired()
+          .OnDelete(DeleteBehavior.Restrict);
+
+        builder.Ignore(c => c.Thumbnail);
+    }
+}
