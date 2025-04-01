e1eacafc7602fd31907a108117776a12d9b4386f
mahmoudqasem2001 <mahmoodqasim543@gmail.com>
2025-04-01 16:18:46 +0200

    OPS-17 Implement Hotel Mapping Profiles

---
+++ AccommodationBookingPlatform.Application/Mapping/HotelProfile.cs
@@ -0,0 +1,38 @@
+﻿using AccommodationBookingPlatform.Application.Hotels.Create;
+using AccommodationBookingPlatform.Application.Hotels.GetForGuestById;
+using AccommodationBookingPlatform.Application.Hotels.GetForGuestById.GetForManagement;
+using AccommodationBookingPlatform.Application.Hotels.GetForGuestById.Search;
+using AccommodationBookingPlatform.Application.Hotels.GetForGuestById.Update;
+using AccommodationBookingPlatform.Domain.Entities;
+using AutoMapper;
+using Domain.Models;
+
+namespace AccommodationBookingPlatform.Application.Mapping;
+
+public class HotelProfile : Profile
+{
+    public HotelProfile()
+    {
+        CreateMap<PaginatedList<HotelSearchResult>, PaginatedList<HotelSearchResultResponse>>()
+          .ForMember(dst => dst.Items, options => options.MapFrom(src => src.Items));
+
+        CreateMap<PaginatedList<HotelForManagement>, PaginatedList<HotelForManagementResponse>>()
+          .ForMember(dst => dst.Items, options => options.MapFrom(src => src.Items));
+
+        CreateMap<CreateHotelCommand, Hotel>();
+        CreateMap<UpdateHotelCommand, Hotel>();
+
+        CreateMap<Hotel, HotelForGuestResponse>()
+          .ForMember(dst => dst.ThumbnailUrl, options => options.MapFrom(
+            src => src.Thumbnail != null ? src.Thumbnail.Path : null))
+          .ForMember(dst => dst.GalleryUrls, options => options.MapFrom(
+            src => src.Gallery.Select(i => i.Path)));
+
+        CreateMap<HotelSearchResult, HotelSearchResultResponse>()
+          .ForMember(dst => dst.ThumbnailUrl, options => options.MapFrom(
+            src => src.Thumbnail != null ? src.Thumbnail.Path : null));
+
+        CreateMap<HotelForManagement, HotelForManagementResponse>()
+          .ForMember(dst => dst.Owner, options => options.MapFrom(src => src.Owner));
+    }
+}
---
+++ AccommodationBookingPlatform/Mapping/HotelProfile.cs
@@ -0,0 +1,34 @@
+﻿using AccommodationBookingPlatform.Application.Hotels.Create;
+using AccommodationBookingPlatform.Application.Hotels.GetFeaturedDeals;
+using AccommodationBookingPlatform.Application.Hotels.GetForGuestById.GetForManagement;
+using AccommodationBookingPlatform.Application.Hotels.GetForGuestById.Search;
+using AccommodationBookingPlatform.Application.Hotels.GetForGuestById.Update;
+using AccommodationBookingPlatform.Application.Hotels.GetRecentlyVisited;
+using AccommodationBookingPlatform.Domain.Enums;
+using AccommodationBookingPlatform.DTOs.Hotels;
+using AutoMapper;
+using static AccommodationBookingPlatform.Mapping.MappingUtilities;
+
+namespace AccommodationBookingPlatform.Mapping;
+
+public class HotelProfile : Profile
+{
+    public HotelProfile()
+    {
+        CreateMap<RecentlyVisitedHotelsGetRequest, GetRecentlyVisitedHotelsForGuestQuery>();
+
+        CreateMap<HotelsGetRequest, GetHotelsForManagementQuery>()
+          .ForMember(dst => dst.SortOrder, opt => opt.MapFrom(src => MapToSortOrder(src.SortOrder)));
+
+        CreateMap<HotelSearchRequest, SearchForHotelsQuery>()
+          .ForMember(dst => dst.SortOrder, opt => opt.MapFrom(src => MapToSortOrder(src.SortOrder)))
+          .ForMember(dst => dst.RoomTypes, opt => opt.MapFrom(src => src.RoomTypes ?? Enumerable.Empty<RoomType>()))
+          .ForMember(dst => dst.Amenities, opt => opt.MapFrom(src => src.Amenities ?? Enumerable.Empty<Guid>()));
+
+        CreateMap<HotelFeaturedDealsGetRequest, GetHotelFeaturedDealsQuery>();
+
+        CreateMap<HotelCreationRequest, CreateHotelCommand>();
+
+        CreateMap<HotelUpdateRequest, UpdateHotelCommand>();
+    }
+}
